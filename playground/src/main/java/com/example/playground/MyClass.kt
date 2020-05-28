f0c69a1175b098759ff14dc6155f97c2f9308ad7
Andras Nemeth <guni82hu@gmail.com>
2020-05-28 22:12:12 +0200

    initial commit

---
+++ playground/src/main/java/com/example/playground/MyClass.kt
@@ -0,0 +1,16 @@
+package com.example.playground
+
+
+fun main(args: Array<String>) {
+    MyClass().sayHello()
+}
+
+class MyClass {
+
+    fun sayHello() {
+        println("Hello Worlddddeeeeee!")
+
+    }
+
+}
+
