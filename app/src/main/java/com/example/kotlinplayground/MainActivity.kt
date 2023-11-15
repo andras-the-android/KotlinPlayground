e3de2a7983cd13ea37f69ec60b8cca293c7f40ac
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-16 00:16:03 +0100

    update coroutines

--- app/src/main/java/com/example/kotlinplayground/MainActivity.kt
+++ app/src/main/java/com/example/kotlinplayground/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loggerAdapter = { s -> Log.d("xxx", s) }
-        Coroutines().jobb()
+        Coroutines().exception()
     }
 }
--- playground/src/main/java/com/example/playground/Coroutines.kt
+++ playground/src/main/java/com/example/playground/Coroutines.kt
@@ -1,12 +1,36 @@
 package com.example.playground
 
-import kotlinx.coroutines.*
-import java.lang.IllegalStateException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.suspendCoroutine
 
+/**
+ * See also (4 part series) https://medium.com/androiddevelopers/coroutines-first-things-first-e6187bf3bb21
+ */
 class Coroutines {
 
+//    start - main
+//    start1 - Thread-0
+//    stop - main
+//    ended - main
+//    start2 - Thread-1
+//    stop2 - Thread-1
+//    stop1 - Thread-0
     fun runThread() {
         prnt("start")
         Thread {
@@ -23,6 +47,13 @@ class Coroutines {
 
     }
 
+//    start - main
+//    start1 - pool-1-thread-1
+//    stop - main
+//    stop1 - pool-1-thread-1
+//    start2 - pool-1-thread-1
+//    stop2 - pool-1-thread-1
+//    ended - main
     fun runExecutors() {
         prnt("start")
         val executor = Executors.newSingleThreadExecutor()
@@ -49,7 +80,7 @@ class Coroutines {
 //    start2 - main
 //    stop2 - main
 //    stop1 - main
-//    rb stop - main  //runBlocking block finishes only after all the local coroutines finished
+//    rb stop - main  // runBlocking block finishes only after all the local coroutines finished
     fun run1() {
         runBlocking<Unit> {
             prnt("start")
@@ -58,7 +89,7 @@ class Coroutines {
                 delay(1000L)
                 prnt("stop1")
             }
-            launch {
+            coroutineScope {
                 prnt("start2")
                 delay(500L)
                 prnt("stop2")
@@ -91,9 +122,16 @@ class Coroutines {
             prnt("stop")
         }
         prnt("rb stop")
-        Thread.sleep(2000) //need to keep jvm alive
+        Thread.sleep(2000) // need to keep jvm alive
     }
 
+//    start - main
+//    start rb - main
+//    Hello, - main
+//    World - DefaultDispatcher-worker-2
+//    ! - main
+//    stop - main
+//    ended - main
     fun join() {
         prnt("start")
         runBlocking<Unit> { // start main coroutine
@@ -110,11 +148,16 @@ class Coroutines {
         prnt("stop")
     }
 
-    /**
-     * According to the documentation runBlocking blocks the thread and coroutineScope doesn't.
-     * But despite this task1 finishes first I don't undersatnd why
-     */
-    fun scope() {
+//    39  start - main
+//    50  stop - main
+//    51  task 1 started - main
+//    54  task 2 started - main
+//    2059  task 2 finished - main
+//    2062  task 1 finished - main
+//    2062  ended - main
+//    for two seconds, everything stops because Thread.sleep - unlike delay -
+//    blocks the entire thread, including the other coroutine
+    fun threadSleep() {
         runBlocking { // this: CoroutineScope
             prnt("start")
             launch {
@@ -123,7 +166,7 @@ class Coroutines {
                 prnt("task 1 finished")
             }
 
-            runBlocking { // Creates a coroutine scope
+            launch { // Creates a coroutine scope
                 prnt("task 2 started")
                 Thread.sleep(2000L)
                 prnt("task 2 finished")
@@ -133,55 +176,251 @@ class Coroutines {
         }
     }
 
-    private var customScope = MainScope()
-
-    fun aaaaa() = runBlocking<Unit> {
+//    48  job1: I run in GlobalScope and execute independently! - DefaultDispatcher-worker-1
+//    154  job2: I am a child of the request coroutine - main
+//    548  call cancel - main
+//    1062  job1: I am not affected by cancellation of the request - DefaultDispatcher-worker-1
+//    1557  ended - main
+    fun cancellation() = runBlocking<Unit> {
         // launch a coroutine to process some kind of incoming request
-        val request = launch {
+        val job = launch {
             // it spawns two other jobs, one with GlobalScope
             GlobalScope.launch {
-                println("job1: I run in GlobalScope and execute independently!")
+                prnt("job1: I run in GlobalScope and execute independently!")
                 delay(1000)
-                println("job1: I am not affected by cancellation of the request")
+                prnt("job1: I am not affected by cancellation of the request")
             }
             // and the other inherits the parent context
             launch {
                 delay(100)
-                println("job2: I am a child of the request coroutine")
+                prnt("job2: I am a child of the request coroutine")
                 delay(1000)
-                println("job2: I will not execute this line if my parent request is cancelled")
+                prnt("job2: I will not execute this line if my parent request is cancelled")
             }
         }
         delay(500)
-        request.cancel() // cancel processing of the request
+        prnt("call cancel")
+        job.cancel() // cancel processing of the request
         delay(1000) // delay a second to see what happens
-        println("main: Who has survived request cancellation?")
     }
 
-    fun bbbb() {
-        GlobalScope.async {
-            throw IllegalStateException("hahaha")
-        }.start()
+    // nulling out the scope won't cancel it
+    fun cancelByScope() = runBlocking {
+        val scope = CoroutineScope(Dispatchers.Default)
+        scope.launch {
+            repeat(10) {
+                prnt("round #$it")
+                delay(500)
+            }
+        }
+        delay(1100)
+        prnt("cancelling...")
+        scope.cancel()
+        delay(5000) // keep jvm alive
     }
 
-    fun jobb() {
-        GlobalScope.launch {
-            val job = launch(Dispatchers.IO) {
-                var counter = 0
-                while(true) {
-                    println("xxx hello ${counter++}")
+    // cancellable functions throw a JobCancellationException when the scope is cancelled
+    // catching this exception can prevent the cancellation of the job
+    fun cancellationException() = runBlocking {
+        val job = launch(Dispatchers.Default) {
+            repeat(10) { i ->
+                try {
+                    // print a message twice a second
+                    prnt("job: I'm sleeping $i ...")
+                    cancellableDelayFunction()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    // removing this line would prevent the cancellation
+                    throw e
                 }
             }
-            println("xxx kill")
-            job.cancel()
+        }
+        delay(1300L) // delay a bit
+        prnt("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        prnt("main: Now I can quit.")
+    }
+
+    private suspend fun cancellableDelayFunction() {
+        // throws JobCancellationException if the scope is cancelled
+        yield()
+        // delay is cancellable by itself so it's not good for this demonstration
+        // unless we call it from this special context
+        withContext(NonCancellable) {
+            delay(500)
+        }
+    }
+
+    // coroutine builders can return a result
+    fun result() {
+        val result = runBlocking {
+            return@runBlocking "hello"
+        }
+        prnt("Result is $result")
+    }
+
+//    25  start - main
+//    39  start1 - main
+//    243  call start job two - main
+//    243  start2 - main
+//    1044  stop1 - main
+//    1249  stop2 - main
+//    1252  The answer is 42 - main
+//    1253  ended - main
+    fun async() = runBlocking {
+        prnt("start")
+        // this one will start instantly
+        val one = async {
+            prnt("start1")
+            delay(1000)
+            prnt("stop1")
+            return@async 40
+        }
+        // this one will run after Deferred.start call
+        val two = async(start = CoroutineStart.LAZY) {
+            prnt("start2")
+            delay(1000)
+            prnt("stop2")
+            return@async 2
+        }
+        delay(200)
+        prnt("call start job two")
+        two.start() // start the second one
+        prnt("The answer is ${one.await() + two.await()}")
+    }
+
+    private val scope = CoroutineScope(Dispatchers.Unconfined)
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        prnt("CoroutineExceptionHandler got $exception")
+    }
+
+    fun exception() {
+        scope.launch(handler) {
+            try {
+                // putting an exception handler here would do nothing because it has a parent
+                launch {
+                    delay(50)
+                    prnt("throwing ex1")
+                    throw Exception("ex1")
+                }
+                launch {
+                    try {
+                        delay(100000)
+                        prnt("finishing")
+                    } finally {
+                        // this one is cancelled because of the the exception on the other coroutine
+                        prnt("cancelled")
+                    }
+                }
+            } catch (e: Exception) {
+                // this wont be called
+            }
+
         }
 
+        // keep the jvm alive.
+        // Without that, the CoroutineExceptionHandler on the root element would never be invoked
+        Thread.sleep(300)
+    }
+
+    fun exception2() {
+        try {
+            // runBlocking passes through the exception
+            runBlocking {
+                launch {
+                    delay(50)
+                    prnt("throwing ex1")
+                    throw Exception("ex1")
+                }
+            }
+        } catch (e: Exception) {
+            prnt("caught")
+        }
+    }
+
+    fun exception3() {
+        scope.launch(handler) {
+            val deferred = async(start = CoroutineStart.LAZY) {
+                throw Exception("ex1")
+            }
+            try {
+                deferred.await()
+            } catch (e: Exception) {
+                // unlike launch, this exception is catchable but it cancels the scope anyway
+                prnt("caught")
+            }
+        }
+    }
+
+    fun exception4() {
+        scope.launch(handler) {
+            val deferred = async(start = CoroutineStart.LAZY) {
+                delay(1000)
+            }
+            deferred.cancel()
+            try {
+                // waiting for a cancelled coroutine throws exception well
+                deferred.await()
+            } catch (e: Exception) {
+                prnt("caught")
+            }
+        }
+    }
+
+//    20  Main - main
+//    42  calling coroutineScope - main @coroutine#1
+//    1045  I'm in a coroutine scope - main @coroutine#1
+//    1046  calling launch - main @coroutine#1
+//    1102  Second new coroutine - main @coroutine#3
+//    1555  First new coroutine - main @coroutine#2
+//    1556  ended - main
+
+//    coroutineScope suspends the execution of the caller and starts it's work on the same coroutine
+    fun launchVsCoroutineScope() {
+        prnt("Main")
+        runBlocking {
+            prnt("calling coroutineScope")
+            // "This function returns as soon as the given block and all its children coroutines are completed."
+            coroutineScope {
+                delay(1000)
+                prnt("I'm in a coroutine scope")
+            }
+            prnt("calling launch")
+            launch {
+                delay(500)
+                prnt("First new coroutine")
+            }
+            launch {
+                delay(50)
+                prnt("Second new coroutine")
+            }
+        }
+    }
+}
+
+private fun callbackBasedMethod(callback: (String) -> Unit) {
+    callback("important result")
+}
+
+suspend fun convertCallbacksToCoroutine(): String = suspendCoroutine { continuation ->
+    callbackBasedMethod { result ->
+        continuation.resumeWith(Result.success(result))
     }
 }
 
 fun main() {
-    Coroutines().jobb()
+    Coroutines().exception4()
     prnt("ended")
+    // todo
+    // kipróbálni az exceptionöket android appból is
+    // supervisor job
 }
 
-fun prnt(s: String) = println(s + " - " + Thread.currentThread().name)
+private val startTimestamp = System.currentTimeMillis()
+
+/**
+ * Use this jvm parameter to display the full name of the coroutine
+ * -Dkotlinx.coroutines.debug
+ */
+private fun prnt(s: String) = println("${System.currentTimeMillis() - startTimestamp}  $s - ${Thread.currentThread().name}")
