c79bc44a8f1e0ef7375a029b721f74946bd5a591
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-17 11:35:03 +0100

    function type with receiver update

--- playground/src/main/java/com/example/playground/FunctionTypeWithReceiver.kt
+++ playground/src/main/java/com/example/playground/FunctionTypeWithReceiver.kt
@@ -1,31 +1,41 @@
 package com.example.playground
 
-fun main() {
-    FunctionTypeWithReceiver().run()
+class Decorator(
+    private val strToDecorate: String
+) {
+
+    fun printDecorated1(createPattern: (strToDecorate: String, delimiter: Char) -> String) {
+        val result = createPattern(strToDecorate, DELIMITER)
+        println(result)
+    }
+
+    fun printDecorated2(createPattern: String.(delimiter: Char) -> String) {
+        val result = createPattern(strToDecorate, DELIMITER) //or strToDecorate.block(DELIMITER)
+        println(result)
+    }
+
+    companion object {
+        private const val DELIMITER = '-'
+    }
 }
 
-class FunctionTypeWithReceiver {
+/**
+ * see also: Type Safe Builders
+ * https://kotlinlang.org/docs/type-safe-builders.html
+ */
 
+fun main() {
+    val d = Decorator("Function type with receiver")
 
-//    aaa bbb
-//    aaa bbb
-    fun run() {
-        transformStringReceiver("aaa") {
-            "$this bbb"
-        }
-
-        transformStringNormal("aaa") {
-            "$it bbb"
-        }
+    // normal function parameter with two arguments
+    // *** - Function type with receiver - ***
+    d.printDecorated1 { strToDecorate, delimiter ->
+        return@printDecorated1 "*** $delimiter $strToDecorate $delimiter ***"
     }
 
-    private fun transformStringReceiver(s: String, action: String.() -> String) {
-        log(s.action())
+    // same functionality with receiver type. strToDecorate is referenced by this keyword
+    // ### - Function type with receiver - ###
+    d.printDecorated2 { delimiter ->
+        return@printDecorated2 "### $delimiter $this $delimiter ###"
     }
-
-    private fun transformStringNormal(s: String, action: (String) -> String) {
-        log(action(s))
-    }
-
-
 }
