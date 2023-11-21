eab9ecccab66c99d8a90bfdb292b408a44265aad
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-21 19:22:19 +0100

    update for properties

--- playground/src/main/java/com/example/playground/Properties.kt
+++ playground/src/main/java/com/example/playground/Properties.kt
@@ -1,45 +1,77 @@
 package com.example.playground
 
-class Properties {
+/**
+ * In Kotlin we declare properties instead of fields. Properties may or may not use fields.
+ */
+open class Properties {
+
+    init {
+        log("initBlock")
+    }
 
     var someValue = 5
 
     // compile error
     // var lazyp: Int by lazy {
-    val lazyp: Int by lazy {
-        log("lazy")
+    val lazyProp: Int by lazy {
+        log("lazy property initialized")
         someValue * 2
     }
 
-    val withoutInit get() = this.someValue * 2
-    // compile error
-    // var withoutInit get() = this.someValue * 2
-
     var initBlock = run {
-        log("initBlock")
+        log("property init block")
         5
     }
 
-    var customAccessor: Int = 0
+    val customImmutableAccessorWithoutBackingField get() = this.someValue * 2
+
+    open var customMutableAccessorWithoutBackingField: Int
+        get() {
+            return 666
+        }
+        set(value) {
+            // no warning if there is no meaningful operation here
+        }
+
+    var customAccessorWithBackingField: Int = 0 // the initializer assigns the backing field directly
         get() {
             log("computed get $field")
+            // backing field is ignored
             return someValue
         }
         set(value) {
             log("computed set $value")
+            // it's not compile error to skip setting backing field but it can break the functionality
             field = value
         }
+
+    private var backingProperty: String? = null
+    var customAccessorWithBackingProperty: Int
+        get() = backingProperty?.toInt() ?: 0
+        set(value) { backingProperty = value.toString()}
+
+    var setterVisibility: String = "abc"
+        private set // the setter is private and has the default implementation
+
+    var setterWithAnnotation: Any? = null
+        @Deprecated("") set // apply annotation on only one accessor
+
+}
+
+class DerivedProperties: Properties() {
+
+    // we can override a property with custom accessor with a plain property
+    override var customMutableAccessorWithoutBackingField: Int = 5
 
 }
 
 fun main() {
     val p = Properties()
     log("initialized")
-    p.customAccessor
+    p.customAccessorWithBackingField
+    p.customAccessorWithBackingField = 55
+    log("customAccessor get returns ${p.customAccessorWithBackingField}")
     p.initBlock
-    p.lazyp
-    log("without init 1 ${p.withoutInit}")
-    p.someValue = 10
-    log("without init 2 ${p.withoutInit}")
+    p.lazyProp
 
 }
