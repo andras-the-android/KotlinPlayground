cf7061119029bc0fe260f77dbfbefc6b19801448
Andras Nemeth <guni82hu@gmail.com>
2023-12-12 12:20:12 +0100

    add prnt to other

--- playground/src/main/java/com/example/playground/other.kt
+++ playground/src/main/java/com/example/playground/other.kt
@@ -1,7 +1,16 @@
 package com.example.playground
 
+import kotlinx.coroutines.runBlocking
 
 
-fun main() {
+fun main() = runBlocking {
 
 }
+
+private val startTimestamp = System.currentTimeMillis()
+
+/**
+ * Use this jvm parameter to display the full name of the coroutine
+ * -Dkotlinx.coroutines.debug
+ */
+private fun prnt(s: String) = println("${System.currentTimeMillis() - startTimestamp}  $s - ${Thread.currentThread().name}")
